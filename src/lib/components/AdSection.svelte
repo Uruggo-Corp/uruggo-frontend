668183f18cb05097cdedfffd3b688c14b91485bc
n3-rd <godwinjdayo@gmail.com>
2023-04-14 22:57:54 +0100

    added ad section

---
+++ src/lib/components/AdSection.svelte
@@ -0,0 +1,12 @@
+<h2 class="ad">Ad here</h2>
+
+<style lang="scss">
+	.ad {
+		height: 135px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 3.35rem;
+		font-weight: 400;
+	}
+</style>
