3b53e12cf50ea29c330be03ae9d342c03b2ce697
n3-rd <godwinjdayo@gmail.com>
2023-04-15 16:29:27 +0100

    added reccomended apartments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import AdSection from '$lib/components/AdSection.svelte';
 	import AvailableApartments from '$lib/components/AvailableApartments.svelte';
+	import ReccomendedAssignments from '$lib/components/ReccomendedAssignments.svelte';
 	import SearchBox from '$lib/components/SearchBox.svelte';
 </script>
 
 <AdSection />
 <SearchBox />
 <AvailableApartments />
+<ReccomendedAssignments />
