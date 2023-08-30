b8bdcbc39ec18b9799c39f79f4560c283778f5f8
Sheyzi <gistkiosk@gmail.com>
2023-08-30 19:23:03 +0100

    Added loader while navigating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import 'iconify-icon';
 	import { Toaster } from 'svelte-french-toast';
 	import { currentUser } from '$lib/stores/users';
-	import { page } from '$app/stores';
+	import { navigating, page } from '$app/stores';
 	import { showToastr } from '$lib/utils';
 	export let data;
 
@@ -25,3 +25,14 @@
 
 <Toaster />
 <slot />
+
+{#if !!$navigating}
+	<div
+		class="z-[9999] fixed h-screen w-screen text-primary flex justify-center items-center top-0 left-0"
+	>
+		<div class="absolute top-0 left-0 w-full h-full bg-black/30" />
+		<div class="p-5 z-50 bg-dark">
+			<iconify-icon icon="eos-icons:bubble-loading" width="40" s />
+		</div>
+	</div>
+{/if}
